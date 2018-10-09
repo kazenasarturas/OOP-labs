84d3e197fe3c226bd0bcc43ada5724c2a20bf983
kazenasarturas <43984321+kazenasarturas@users.noreply.github.com>
2018-10-09 14:14:33 +0100

    Update Seismometer_v3.h

--- Seismometer_v3.h
+++ Seismometer_v3.h
@@ -9,20 +9,20 @@
 
 class Seismometer{
 private:
-	int data;				//Number of data elements
-	double time_incr;		//Time increment
-	double *sensor;			//To read in data elements 
+	int data;		//Number of data elements
+	double time_incr;	//Time increment
+	double *sensor;		//To read in data elements 
 	double *Short_time_P;	//Short-time power
 	double *Long_time_P;	//Long-time power
-	double *Ratio;			//Ratio between powers
+	double *Ratio;		//Ratio between powers
 	int Short_W_Samples;	//Number of samples chossen for short-window
-	int Long_W_Samples;		//Number of samples chossen for long-window
-	double Threshold;		//Threshold value for siesmic data
+	int Long_W_Samples;	//Number of samples chossen for long-window
+	double Threshold;	//Threshold value for siesmic data
 
 public:
 	Seismometer(){};
-	void ReadFile();		//To read in data from file
-	void DetectQuake();		//To perform calculations and detect possible quakes
+	void ReadFile();	//To read in data from file
+	void DetectQuake();	//To perform calculations and detect possible quakes
 };
 
 
